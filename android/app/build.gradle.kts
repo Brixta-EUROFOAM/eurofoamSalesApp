ac58fc135868831cbd8ff14c30f636b934347316
Zaheer Abbas <zaheerabbas@Zaheers-MacBook-Air.local>
2025-11-15 03:46:25 +0530

    xuddho grablebuild file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,16 @@
-def keystorePropertiesFile = rootProject.file('../key.properties')
-def keystoreProperties = new Properties()
+import java.util.Properties
+import java.io.FileInputStream
+
+
+val keystorePropertiesFile = rootProject.projectDir.resolve("key.properties")
+val keystoreProperties = Properties()
 if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services") 
 }
@@ -29,16 +32,27 @@ android {
 
     defaultConfig {
         applicationId = "com.example.assetarchiverflutter"
-        minSdk = maxOf(21, flutter.minSdkVersion) // make sure it’s >= 21
+        minSdk = maxOf(21, flutter.minSdkVersion) 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            if (keystoreProperties.getProperty("storeFile") != null) {
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+            }
+        }
+    }
+
 
     buildTypes {
-        release {
-            signingConfig signingConfigs.release
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
@@ -48,7 +62,6 @@ flutter {
 }
 
 dependencies {
-    // Your other dependencies go here
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
     implementation("com.google.firebase:firebase-analytics")
