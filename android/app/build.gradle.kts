69edd8913bc8557737bdc5c9ac7b7c726c590189
goswami-rohit <rohit.second44@gmail.com>
2026-05-15 12:22:40 +0530

    dvr/pjp/attendance submit works

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,6 +63,6 @@ flutter {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
     implementation("com.google.firebase:firebase-analytics")
 }
